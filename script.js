ca3fb64a040e561e8af3955d393b0b125996be06
Samuel Granados <samuelgranados2801@gmail.com>
2023-12-09 21:50:47 -0300

    script barra de pesquisa buscar

--- script.js
+++ script.js
@@ -1,22 +1,51 @@
 const containerVideos = document.querySelector(".videos__container");
 
-const api = fetch("http://localhost:3000/videos")
-.then(res => (res.json()))
-.then((videos) =>
+async function buscarEMostrarVideos() {
+  try{
+ const busca = await fetch("http://localhost:3000/videos");
+ const videos = await busca.json();
+
     videos.forEach((video) => {
+      if(video.categoria == "") {
+        throw new Error('Vídeo não tem categoria');
+      }
       containerVideos.innerHTML += `
       <li class="videos__item">
       <iframe src="${video.url}" title="${video.titulo}" frameborder="0" allowfullscreen></iframe>
       <div class="descricao-video">
       <img class="img-canal" src="${video.imagem}" alt="logo do canal">
-      <h3 class="titulo_video">${video.titulo}</h3>
+      <h3 class="titulo-video">${video.titulo}</h3>
       <p class="titulo-canal">${video.descricao}</p>
       </div>
       </li>
       `;
     })
-)
-.catch((error)=>{
-  containerVideos.innerHTML = `<p> Houve um erro ao carregar os vídeos: ${error} </p>`
-})
+  } catch(error){
+    containerVideos.innerHTML = `<p>Houve um erro ao carregar os vídeos: ${error}</p>`
+  }
+}
 
+buscarEMostrarVideos();
+
+const barraDePesquisa = document.querySelector(".pesquisar__input");
+
+barraDePesquisa.addEventListener("input", filtrarPesquisa);
+
+function filtrarPesquisa() {
+  const videos = document.querySelectorAll(".videos__item");
+
+  if(barraDePesquisa != "") {
+    for(let video of videos){
+      let titulo = video.querySelector(".titulo-video").textContent.toLowerCase();
+      let valorFiltro = barraDePesquisa.value.toLowerCase();
+
+      if(!titulo.includes(valorFiltro)){
+        video.style.display = "none";
+      } else {
+        video.style.display = "block";
+      }
+    }
+  } else {
+    video.style.display = "block";
+  }
+}
